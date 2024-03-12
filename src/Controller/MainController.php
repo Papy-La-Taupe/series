475e5d959e21419fb15b5ac0a41a6e9c0036c732
Papy-La-Taupe <c.combes.pro@gmail.com>
2024-03-12 15:32:18 +0100

    Add : inheritance notion & base.html.twig
    Allow us to test how great heritage can be.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,12 +10,10 @@ class MainController extends AbstractController
     #[Route(path: '/', name: 'main_home')]
     public function home(){
         return $this->render('main/home.html.twig');
-        die();
     }
 
     #[Route(path: '/test', name: 'main_test')]
     public function test(){
         return $this->render('main/test.html.twig');
-        die();
     }
 }
